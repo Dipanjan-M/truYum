7f94d8d98fd45f0a083a1b36ba3b0d651a934e18
Dipanjan Maity <55407059+Dipanjan-M@users.noreply.github.com>
2021-03-23 14:49:44 +0530

    Added database schema and queries

---
+++ truyum-ui/sql/truyum-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `menu_item`(`name`, `price`, `status`, `date_of_launch`, `category`, `free_delivery`) VALUES 
+('Sandwich',99.00,'Yes','2017-02-15','Main Course','Yes'),
+('Burger',129.00,'Yes','2017-12-23','Main Course','No'),
+('Pizza',149.00,'Yes','2017-08-21','Main Course','No'),
+('French Fries',57.00,'No','2017-07-02','Starter','No'),
+('Chocolate Brownie',32.00,'Yes','2022-11-02','Dessert','Yes');
